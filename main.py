3e5eedf6c8d5251a0ea1362575391709f0254ace
yahyashanaah <yahya.shanaah@gmail.com>
2024-11-06 22:46:15 +0400

    Valid Parentheses

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-command = ""
-while True:
-    command = input("> ").lower()
-    if command == "start":
-        print("Car started...")
-    elif command == "stop":
-        print("Car stopped...")
-    elif command == "help":
-        print("""
-        start - to start the car
-        stop - to stop the car
-        quit - to exit
-        """)
-    elif command == "quit":
-        break
-    else:
-        print("Sorry, I don't understand that")
+# nums = [2, 7, 11, 15]
+# result = nums[0] + nums[1]
+# print(result)
+
+# input = ["s", "t", "r", "i", "n", "g"]	
+
+# def reverse_string(input):
+#     return input[::-1]
+
+# result = reverse_string(input)
+# print(result)
+
+s = "()[]{}"
+def isValid(s):
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    return not stack
+
+result = isValid(s)
+print(result)

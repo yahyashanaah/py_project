aec7668b46f4f2f372d13b1dc8570ab78bc46de9
yahyashanaah <yahya.shanaah@gmail.com>
2024-11-07 21:48:05 +0400

    testing7nov

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
-# nums = [2, 7, 11, 15]
-# result = nums[0] + nums[1]
-# print(result)
 
-# input = ["s", "t", "r", "i", "n", "g"]
+x = 5
+y = 5
 
-# def reverse_string(input):
-#     return input[::-1]
-
-# result = reverse_string(input)
-# print(result)
-
-# s = "()[]{}"
-# def isValid(s):
-#     stack = []
-#     mapping = {")": "(", "}": "{", "]": "["}
-#     for char in s:
-#         if char in mapping:
-#             top_element = stack.pop() if stack else '#'
-#             if mapping[char] != top_element:
-#                 return False
-#         else:
-#             stack.append(char)
-#     return not stack
-
-# result = isValid(s)
-# print(result)
-
-# nums = [3, 0, 1]
-
-
-# def missingNumber(nums):
-#     n = len(nums)
-#     return n * (n + 1) // 2 - sum(nums)
-
-
-# result = missingNumber(nums)
-# print(result)
+for i in range(x):
+    print("x" * x)
+    for j in range(2):
+        print("x" + " " * (x-2) + "x")
+        print("x" * x)
+        

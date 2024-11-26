a984a7b4fd06dceaa3a64c3c4cb41bbddc77c107
yahyashanaah <yahya.shanaah@gmail.com>
2024-11-26 12:03:27 +0400

    update sheet

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import openpyxl as xl
-
+from openpyxl.chart import BarChart, Reference
 # Load the workbook
-wb = xl.load_workbook('sheet1.xlsx')
 
-# Access the sheet by its name
-sheet = wb['Sheet1']
+def proccess_workbook(filename):
+    wb = xl.load_workbook(filename)
 
-# Access a specific cell
-cell = sheet.cell(1, 1)
-print(sheet.max_row)
+    # Access the sheet by its name
+    sheet = wb['Sheet1']
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    if isinstance(cell.value, (int, float)):  # Check if the cell value is numeric
-        corrected_price = cell.value * 0.9
-        corrected_price_cell = sheet.cell(row, 4)  # Create a new cell
-        corrected_price_cell.value = corrected_price  # Assign the value to the new cell
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        if isinstance(cell.value, (int, float)):  # Check if the cell value is numeric
+            corrected_price = cell.value * 0.9
+            corrected_price_cell = sheet.cell(row, 4)  # Create a new cell
+            corrected_price_cell.value = corrected_price  # Assign the value to the new cell
+        
+    values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4) # Create a reference object
 
-# Save the workbook
-wb.save('NewListPrice.xlsx')
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')  
+    # Save the workbook
+    wb.save(filename)
